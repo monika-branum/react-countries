d0ad2cd82c86d955fde2f01bf99755d39d522e79
Monika <moncobra@comcast.net>
2023-01-18 15:28:15 -0800

    created use countries function

---
+++ src/hooks/useCountries.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import fetchCountries from '../services/countries.js';
+
+export default function useCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchCountries();
+      setCountries(resp);
+    };
+    fetchData();
+  }, []);
+  return countries;
+}
