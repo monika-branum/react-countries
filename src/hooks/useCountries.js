d4975acffcd8febaba5a7a786c61f96fc966e2a9
Monika <moncobra@comcast.net>
2023-01-23 18:16:41 -0800

    test3

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import fetchCountries from '../services/countries.js';
 
 export default function useCountries() {
--- src/services/countries.js
+++ src/services/countries.js
@@ -2,6 +2,6 @@ import { checkError, client } from './client.js';
 
 export default async function fetchCountries() {
   const resp = await client.from('countries').select('*');
-  console.log(resp, 'consolelog resp');
+
   return checkError(resp);
 }
